044b9cb9221dffa6df9aa310099b5f3183974088
JuanDiego98 <jlozanoc@unal.edu.co>
2019-11-04 17:29:39 -0500

    Test de separación de capa de presentación

--- mobile-app/app/src/main/java/co/edu/unal/se1/presentation/activity/MainActivity.java
+++ mobile-app/app/src/main/java/co/edu/unal/se1/presentation/activity/MainActivity.java
@@ -43,30 +43,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final TextView sourceIdInput = findViewById(R.id.sourceId);
-        final TextView targetIdInput = findViewById(R.id.targetId);
-        final TextView valueInput = findViewById(R.id.value);
-
-        Button sendMoneyButton = findViewById(R.id.sendMoneyButton);
-        sendMoneyButton.setOnClickListener(new View.OnClickListener() {
+        Button tButton = findViewById(R.id.tButton);
+        tButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-
-                userController = new UserController();
-
-                int sourceId = Integer.parseInt(sourceIdInput.getText().toString());
-                int targetId = Integer.parseInt(targetIdInput.getText().toString());
-                double value = Double.parseDouble(valueInput.getText().toString());
-
-                boolean transaction = userController.sendMoney(sourceId, targetId, value, getApplicationContext());
-
-                if (transaction) {
-                    System.out.println("¡Transacción satisfactoria!");
-                } else {
-                    System.out.println("¡Transacción no satisfactoria!");
-                }
+            public void onClick(View v){
+                openTransaction();
             }
         });
+
+    }
+
+    public void openMain(){
+        Intent intent = new Intent(this,TransactionActivity.class);
+        startActivity(intent);
     }
 
 }
---
+++ mobile-app/app/src/main/java/co/edu/unal/se1/presentation/activity/TransactionActivity.java
@@ -0,0 +1,68 @@
+package co.edu.unal.se1.presentation.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import co.edu.unal.se1.R;
+import co.edu.unal.se1.businessLogic.controller.UserController;
+import co.edu.unal.se1.dataAccess.model.User;
+import co.edu.unal.se1.dataAccess.repository.UserRepository;
+
+public class MainActivity extends AppCompatActivity {
+
+    private UserController userController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction);
+
+        final TextView sourceIdInput = findViewById(R.id.sourceId);
+        final TextView targetIdInput = findViewById(R.id.targetId);
+        final TextView valueInput = findViewById(R.id.value);
+
+        Button sendMoneyButton = findViewById(R.id.sendMoneyButton);
+        sendMoneyButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                userController = new UserController();
+
+                int sourceId = Integer.parseInt(sourceIdInput.getText().toString());
+                int targetId = Integer.parseInt(targetIdInput.getText().toString());
+                double value = Double.parseDouble(valueInput.getText().toString());
+
+                boolean transaction = userController.sendMoney(sourceId, targetId, value, getApplicationContext());
+
+                if (transaction) {
+                    System.out.println("¡Transacción satisfactoria!");
+                } else {
+                    System.out.println("¡Transacción no satisfactoria!");
+                }
+            }
+        });
+
+        Button uButton = findViewById(R.id.uButton);
+        uButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openMain();
+            }
+        });
+
+    }
+
+    public void openMain(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+}
